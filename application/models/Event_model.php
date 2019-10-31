2cdd4c4a88a2d3563f764d02b244c048663a08d2
OnyxzeD <hizialuke@gmail.com>
2019-10-31 21:36:09 +0700

    update event api

--- application/models/Event_model.php
+++ application/models/Event_model.php
@@ -15,15 +15,36 @@ class Event_model extends CI_Model
         $this->load->model('General_model');
     }
 
-    public function detail($url)
+    public function detail($id)
     {
-        $this->db->join('user_bio', 'news.author = user_bio.username');
-        $this->db->select('news.*, user_bio.fullname');
-        $this->db->from('news');
-        $this->db->where('url', $url);
+        $this->db->where('event_id', $id);
+        $query = $this->db->get('events');
+
+        $event = $query->row_array();
+        if ($event['poster'] != null) {
+            $event['poster'] = base_url() . '/assets/images/events/'.$event['poster'];
+        } else {
+            $event['poster'] = base_url() . "assets/dist/img/flyer.png";
+        }
+
+        $this->db->join('schedule', 'events.event_id = schedule.event_id');
+        $this->db->join('artist', 'artist.artist_id = schedule.artist_id');
+        $this->db->select('events.event_id, schedule.show_time, artist.artist_name, artist.country_of_origin');
+        $this->db->from('events');
+        $this->db->where('events.event_id', $id);
+        $this->db->order_by('event_id', 'ASC');
+        $this->db->order_by('show_time', 'ASC');
         $query = $this->db->get();
 
-        return $query->row_array();
+        // reformat data
+        $result = [];
+        $data = $query->result_array();
+        foreach ($data as $dt) {
+            $result[] = $dt;
+        }
+        $event['details'] = $result;
+
+        return $event;
     }
 
     function getData()
@@ -81,11 +102,33 @@ class Event_model extends CI_Model
         $result = [];
         $data = $query->result_array();
         foreach ($data as $dt) {
+            $dt['date_start'] = convertDate($dt['date_start'], 'indo');
+            $dt['date_end'] = convertDate($dt['date_end'], 'indo');
+            $dt['htm'] = 'Rp ' . number_format($dt['htm'], 0, ".", ".");
+
             if ($dt['poster'] != null) {
-                $dt['poster'] = base_url() . '/'.$dt['poster'];
+                $dt['poster'] = base_url() . 'assets/img/'.$dt['poster'];
             } else {
-                $dt['poster'] = base_url() . "assets/dist/img/flyer.png";
+                $dt['poster'] = base_url() . "assets/img/flyer.png";
             }
+
+            $this->db->join('schedule', 'events.event_id = schedule.event_id');
+            $this->db->join('artist', 'artist.artist_id = schedule.artist_id');
+            $this->db->select('events.event_id, schedule.show_time, artist.artist_name, artist.country_of_origin');
+            $this->db->from('events');
+            $this->db->where('events.event_id', $dt['event_id']);
+            $this->db->order_by('event_id', 'ASC');
+            $this->db->order_by('show_time', 'ASC');
+            $query2 = $this->db->get();
+
+            // reformat data
+            $details = [];
+            $detail = $query2->result_array();
+            foreach ($detail as $d) {
+                $details[] = $d;
+            }
+            $dt['details'] = $details;
+
             $result[] = $dt;
         }
 
@@ -94,9 +137,19 @@ class Event_model extends CI_Model
 
     public function jsonDetail($id)
     {
+        $this->db->where('event_id', $id);
+        $query = $this->db->get('events');
+
+        $event = $query->row_array();
+        if ($event['poster'] != null) {
+            $event['poster'] = base_url() . '/assets/img/'.$event['poster'];
+        } else {
+            $event['poster'] = base_url() . "assets/img/flyer.png";
+        }
+
         $this->db->join('schedule', 'events.event_id = schedule.event_id');
         $this->db->join('artist', 'artist.artist_id = schedule.artist_id');
-        $this->db->select('events.*, schedule.show_time, artist.artist_name, artist.country_of_origin');
+        $this->db->select('events.event_id, schedule.show_time, artist.artist_name, artist.country_of_origin');
         $this->db->from('events');
         $this->db->where('events.event_id', $id);
         $this->db->order_by('event_id', 'ASC');
@@ -107,14 +160,10 @@ class Event_model extends CI_Model
         $result = [];
         $data = $query->result_array();
         foreach ($data as $dt) {
-            if ($dt['poster'] != null) {
-                $dt['poster'] = base_url() . '/'.$dt['poster'];
-            } else {
-                $dt['poster'] = base_url() . "assets/dist/img/flyer.png";
-            }
             $result[] = $dt;
         }
+        $event['details'] = $result;
 
-        return $result;
+        return $event;
     }
 }
